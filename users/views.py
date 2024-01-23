06feb3d1111803663256b3a21509aefa8841f70c
Kostia <kostia3507@gmail.com>
2024-01-23 20:14:48 +0200

    added view for user creation

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from users.forms import RegistrationForm
+
+
+def register_view(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = RegistrationForm()
+
+    return render(request, "registration/register.html", {"form": form})
