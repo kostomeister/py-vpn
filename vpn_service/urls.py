2ed9693cfcebe2a694a4afb8880aafb82638b81c
Kostia <kostia3507@gmail.com>
2024-01-25 01:42:47 +0200

    implemented users urls page

--- vpn_service/urls.py
+++ vpn_service/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from vpn_service.views import index, statistics
+from vpn_service.views import index, statistics, user_urls
 
 urlpatterns = [
     path("", index, name="index"),
     path('statistics/', statistics, name='statistics'),
+    path("urls/", user_urls, name="user_urls"),
 ]
 
 app_name = "vpn_service"
--- vpn_service/views.py
+++ vpn_service/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import SiteStatistics
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from .forms import URLForm
+from .models import Site, SiteStatistics
 
 
 def index(request):
@@ -10,3 +13,18 @@ def statistics(request):
     statistics_data = SiteStatistics.objects.filter(user=request.user)
     return render(request, 'vpn_service/statistics.html', {'statistics_data': statistics_data})
 
+
+@login_required
+def user_urls(request):
+    user = request.user
+    user_sites = Site.objects.filter(user=user)
+
+    if request.method == 'POST':
+        form = URLForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('vpn_service:user_urls')
+    else:
+        form = URLForm()
+
+    return render(request, 'vpn_service/user_urls.html', {'user_sites': user_sites, 'form': form})
