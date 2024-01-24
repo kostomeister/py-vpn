bf68284b932cf28615c1f7cccc54c80167a45187
Kostia <kostia3507@gmail.com>
2024-01-25 01:37:28 +0200

    implemented statistics page

--- vpn_service/urls.py
+++ vpn_service/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from vpn_service.views import index
+from vpn_service.views import index, statistics
 
 urlpatterns = [
     path("", index, name="index"),
+    path('statistics/', statistics, name='statistics'),
 ]
 
 app_name = "vpn_service"
--- vpn_service/views.py
+++ vpn_service/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import SiteStatistics
 
 
 def index(request):
     return render(request, 'vpn_service/index.html')
+
+
+def statistics(request):
+    statistics_data = SiteStatistics.objects.filter(user=request.user)
+    return render(request, 'vpn_service/statistics.html', {'statistics_data': statistics_data})
+
