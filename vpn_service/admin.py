ec2e0cc3dfc9d9e20e3aa6c7d6442e4b7277189d
Kostia <kostia3507@gmail.com>
2024-01-25 01:33:46 +0200

    added vpn_service models to admin page

--- vpn_service/admin.py
+++ vpn_service/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Site, SiteStatistics
 
-# Register your models here.
+
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ("user", 'name', 'url')
+    search_fields = ("user", 'name', 'url')
+
+
+@admin.register(SiteStatistics)
+class SiteStatisticsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'site', 'data_size', 'page_transitions_count')
+    list_filter = ('user', 'site')
+    search_fields = ('user__username', 'site__name')
