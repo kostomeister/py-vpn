d5b0e0f480f4c8e2cfbaec97d25227265b02a435
Kostia <kostia3507@gmail.com>
2024-01-25 01:32:14 +0200

    included users urls to config urls

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("register/", include("users.urls", namespace="users"))
+    path("register/", include("users.urls", namespace="users")),
 ]
