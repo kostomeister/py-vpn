f42707708fbb9ad5cbcb8b2c2ffe58044d9af089
Kostia <kostia3507@gmail.com>
2024-01-25 01:29:23 +0200

    removed unneccessary fields from user model

--- users/models.py
+++ users/models.py
@@ -3,6 +3,4 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    phone_number = models.CharField(max_length=15, null=True, blank=True)
-    country = models.CharField(max_length=50, null=True, blank=True)
+    pass
