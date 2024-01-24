57ed7d0e2226bb249ae7588339700ebb96cde37f
Kostia <kostia3507@gmail.com>
2024-01-25 01:29:01 +0200

    added form for updating user

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,17 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
+
+
+class UserUpdateForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+        ]
 
 
 class RegistrationForm(UserCreationForm):
