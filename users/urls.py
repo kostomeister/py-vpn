1617020d4b01872bc6ede76e87b414f8205eff44
Kostia <kostia3507@gmail.com>
2024-01-25 01:31:54 +0200

    realized profile updating logic

--- users/urls.py
+++ users/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 from django.urls import path
 
-from users.views import register_view, ProfileView
+from users.views import register_view, ProfileUpdateView, ProfileView
 
 urlpatterns = [
     path("", register_view, name="register"),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
-
+    path('profile/<int:pk>/edit/', ProfileUpdateView.as_view(), name='profile_edit'),
 ]
 
 app_name = "users"
